11de83eede9c75b893669942b763ad3c64a3ea32
Mujhekya <tiwariyash148@gmail.com>
2025-07-28 15:22:16 +0000

    variable done

---
+++ basics/var.js
@@ -0,0 +1,17 @@
+const account_ID = 15573
+let account_Email = "yash@gmail.com"
+var account_Password = "12345"
+account_City = "jaipur"
+
+// account_ID = 4
+account_Email ="oye@ublubublu.com"
+account_Password = "bhagbe"
+account_City = "satna"
+
+/*
+do not prefer to use var 
+beacuse of problems within block scope and functional scope
+*/
+
+console.log(account_ID)
+console.table([account_ID,account_Email,account_Password,account_City])
